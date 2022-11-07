48f3ac44a0968f0956926e13196a71d38154accb
vincent <vincentpalomo@yahoo.com>
2022-11-06 21:51:50 -0600

    added some js and psuedocode

--- tictactoe.js
+++ tictactoe.js
@@ -1,2 +1,35 @@
-let cells = document.querySelectorAll('.tiles')
+let cells = document.querySelectorAll('.tile')
 console.log(cells)
+
+// const gameState = {
+//     players: ['x', 'o'],
+//     board: [
+//       [null, null, null],
+//       [null, null, null],
+//       [null, null, null]
+//     ]
+//   }
+
+// // state
+// let state;
+
+// function buildInitialState() {
+
+// }
+
+// // render
+// function renderState() {
+
+// }
+
+// // maybe a dozen or so helper functions for tiny pieces of the interface
+
+// // listeners
+// function onBoardClick() {
+//   // update state, maybe with another dozen or so helper functions...
+
+//   renderState() // show the user the new state
+// }
+// const board = document.getElementById('board');
+// board.addEventListener('click', onBoardClick); // etc
+
