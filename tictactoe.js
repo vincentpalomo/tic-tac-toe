eef34220fd77a7daec3abae08d10ce359946ef7c
vincent <vincentpalomo@yahoo.com>
2022-11-08 15:31:17 -0600

    updated change player function not working yet

--- tictactoe.js
+++ tictactoe.js
@@ -4,7 +4,13 @@ let reset = document.querySelector("#reset");
 
 let state = false;
 
-let board = [null, null, null, null, null, null, null, null, null]
+let board = [
+    [null, null, null], 
+    [null, null, null], 
+    [null, null, null]
+]
+
+let players = ["X", "O"]
 
 let currentPlayer = "X"
 
@@ -14,6 +20,7 @@ buildInitialState()
 function buildInitialState() {
   tile.forEach(tile => tile.addEventListener('click', tileClicked))
   console.log('clicked', tileClicked)
+  reset.addEventListener('click', resetGame)
   state = true;
 }
   //function to get the input of the tile clicked
@@ -28,16 +35,44 @@ function tileClicked () {
 function updateTile (tile, index) {
   board[index] = currentPlayer;
   tile.textContent = currentPlayer;
+  changePlayer()
 }
 
-// function changePlayer () {
-//   for {player[i]) 
-// }
+// create function to change between player X and O
+function changePlayer () {
+    for (let i = 0; i < players.length; i++) {
+        let playerX = players[0]
+        let playerO = players[1]
+        // console.log('player x:', playerX)
+        // console.log('player o:', playerO)
+        console.log('currentplayer:', currentPlayer)
+        // currentPlayer = (currentPlayer == "X") ? "O" : "X"; // ternary
+        if (currentPlayer == playerX) {
+            currentPlayer = playerO;
+        } else if (currentPlayer == playerO) {
+           currentPlayer = playerX;
+        }
+    }
 
+}
+
+// changePlayer()
 // render
 // function renderState() {
 
 // }
+
+// function to reset game
+function resetGame () {
+    currentPlayer = "X";
+    board = [
+        [null, null, null], 
+        [null, null, null], 
+        [null, null, null]
+    ];
+    tile.forEach(tile => tile.textContent = "");
+    state = true;
+}
 
 // maybe a dozen or so helper functions for tiny pieces of the interface
 
